f417d5bdfdcf9069e550a0e673ccbd08de0fa19f
Lazene <psr12320@students.ephec.be>
2024-02-13 20:13:28 +0100

    logout

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthentificationService } from '../services/authentification.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
+  isAuthentificated: boolean;
+  private authSubscription: Subscription;
 
+  constructor(private authService: AuthentificationService) {}
+
+  ngOnInit(): void {
+    // Initialiser l'état d'authentification
+    this.isAuthentificated = this.authService.isAuthentificated();
+
+    // S'abonner aux changements de l'état d'authentification
+    this.authSubscription = this.authService.isAuthenticated$.subscribe(
+      isAuthenticated => this.isAuthentificated = isAuthenticated
+    );
+  }
+
+  ngOnDestroy(): void {
+    // Se désabonner lors de la destruction du composant pour éviter les fuites de mémoire
+    this.authSubscription.unsubscribe();
+  }
+
+  logout() {
+    // Appeler la méthode logout() du service d'authentification
+    this.authService.logout();
+  }
 }
--- src/app/services/authentification.service.ts
+++ src/app/services/authentification.service.ts
@@ -1,19 +1,22 @@
 import { HttpClient, HttpEvent } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthentificationService {
+  private isAuthentificatedSubject = new BehaviorSubject<boolean>(false);
 
   constructor(private http : HttpClient, public jwtHelper : JwtHelperService) {
 
    }
    // méthode pour logger un utilisateur
    login (userName: string, password: string): Observable<any>{
-     return this.http.post(`https://localhost:7176/Authentification/Login?password=${password}&login=${userName}`, null);
+    if (this.isAuthentificated){this.isAuthentificatedSubject.next(true);} 
+    return this.http.post(`http://localhost:5244/Authentification/Login?password=${password}&login=${userName}`, null);
+
    }
    // méthode isAuthentificated pour vérifier si un utilisateur est authentifié
    isAuthentificated(): boolean{
@@ -32,6 +35,14 @@ export class AuthentificationService {
   }
 logout(){
   sessionStorage.removeItem("jwt");
+  this.isAuthentificatedSubject.next(false);
 }
-
+// méthode pour retourner l'état de l'authentification
+get isAuthenticated$(): Observable<boolean> {
+  return this.isAuthentificatedSubject.asObservable();
+}
+// methode pour mettre à jour l'état de l'authentification
+updateAuthenticationState(isAuthenticated: boolean): void {
+  this.isAuthentificatedSubject.next(isAuthenticated);
+}
 }
