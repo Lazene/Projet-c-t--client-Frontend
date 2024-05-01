f37759e591a7e5a67dee6daf846bb8a868a3067b
Lazene <psr12320@students.ephec.be>
2024-05-01 17:17:50 +0200

    dashboardstudent

---
+++ src/app/student-dashboard/assignment-list/assignment-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { AssignmentDTO } from '../../shared/DTO/assignmentDto';
+  // Assurez-vous que le chemin d'importation est correct
+
+@Component({
+  selector: 'app-assignment-list',
+  templateUrl: './assignment-list.component.html',
+  styleUrls: ['./assignment-list.component.css']
+})
+export class AssignmentListComponent {
+  @Input() assignments: AssignmentDTO[];  // Recevoir les devoirs du composant parent
+
+  constructor() { }
+
+  // Vous pouvez ajouter d'autres méthodes pour la logique spécifique aux devoirs ici
+}
